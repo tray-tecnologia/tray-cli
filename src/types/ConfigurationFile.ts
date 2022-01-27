64b55a1817c10c6cb716c6ff014e2de253a2d344
Rafael Andrade <rafael.andrade@netzee.com.br>
2022-01-27 09:48:25 -0300

    feat: makes themeId and previewUrl properties optional

--- src/types/ConfigurationFile.ts
+++ src/types/ConfigurationFile.ts
@@ -1,7 +1,7 @@
 export declare type ConfigurationFile = {
     key: string;
     password: string;
-    themeId: number | null;
-    previewUrl: string;
+    themeId?: number;
+    previewUrl?: string;
     debug: boolean;
 };
